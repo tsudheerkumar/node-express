e9c04dda25f57f3d4d2a7e469ffce03cfa9b2e17
Sudheer Kumar Tumarada <stumarada@sapient.com>
2018-01-18 22:09:57 +0530

    added data

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
+var data = require("./data.json");
+var YAML = require('json2yaml')
 
+
+data.forEach(element => {
+    let size = JSON.stringify(element).length;
+    let length = Object.keys(element).length;
+    element['length'] = length;
+    element['size'] = size;
+});
+
+ymlText = YAML.stringify({
+    "foo": "bar"
+  , "baz": "corge"
+  });
+
+  console.log(ymlText);
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
